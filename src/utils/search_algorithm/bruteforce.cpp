c9efda5c0526b235a63f5beb03c195fa2fa6646a
Warzkos <hubertdrozdziak@gmail.com>
2023-11-28 18:35:03 +0100

    wrote tests for bruteforce<double> and fixed bruteforce alghorithm

--- src/utils/search_algorithm/bruteforce.cpp
+++ src/utils/search_algorithm/bruteforce.cpp
@@ -15,7 +15,7 @@ std::vector<std::pair<std::vector<T>, double>> BruteForce<T>::distance_to_kneare
                     k_nearest.push_back(std::make_pair(point, _distance -> calculateDistance(target, point)));
                 }
                 std::sort(k_nearest.begin(), k_nearest.end(), [](auto const &p1, auto const &p2) { return p1.second < p2.second; });
-                k_nearest.erase(k_nearest.begin() + k_nearest.size() - k + 1, k_nearest.end());
+                k_nearest.erase(k_nearest.begin() + k + 1, k_nearest.end());
                 distanceToKNN.push_back(std::make_pair(target, std::accumulate(k_nearest.begin(), k_nearest.end(), 0.0, [](double sum, auto const &p) { return sum + p.second; })));
             }
             return distanceToKNN;
--- tests/search_algorithm_test.cpp
+++ tests/search_algorithm_test.cpp
@@ -9,14 +9,43 @@
 #include "../lib/utils/distance.h"
 #include "../lib/utils/distances/euclidean.h"
 
-// class SearchAlgorithmDoubleTest : public ::testing::Test {
-// protected:
-//     void SetUp() override {
-//         distance = std::make_unique<Euclidean>();
-//     }
-// public:
-//     std::unique_ptr<Distance> distance;
-// };
+class SearchAlgorithmDoubleTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        data = {
+            {2.0, -1.0},
+            {0.5, 1.0},
+            {0.0, -0.5},
+            {-0.8, 0.8},
+            {-1.6, -2.4},
+            {-2.8, -0.8},
+            {-3.0, 1.0},
+            {-3.0, -2.0},
+            {-4.0, -1.5},
+        };
+
+        k_nearest_ans = {
+            {{2.0, -1.0}, 2.06+2.5+3.33},
+            {{0.5, 1.0}, 1.32+1.58+2.5},
+            {{0.0, -0.5}, 1.53+1.58+2.06},
+            {{-0.8, 0.8}, 1.32+1.53+2.21},
+            {{-1.6, -2.4}, 1.46+2.0+2.48},
+            {{-2.8, -0.8}, 1.22+1.39+1.81},
+            {{-3.0, 1.0}, 1.81+2.21+2.69},
+            {{-3.0, -2.0}, 1.12+1.22+1.46},
+            {{-4.0, -1.5}, 1.12+1.39+2.56},
+        };
+
+        distance = std::make_shared<Euclidean>();
+
+        k=3;
+    }
+public:
+    std::shared_ptr<Distance> distance;
+    std::vector<std::vector<double>> data;
+    std::vector<std::pair<std::vector<double>, double>> k_nearest_ans;
+    int k;
+};
 
 class SearchAlgorithmIntTest : public ::testing::Test {
 protected:
@@ -50,10 +79,21 @@ public:
     int k;
 };
 
-TEST_F(SearchAlgorithmIntTest, BruteForceSearchKNearest) {
+TEST_F(SearchAlgorithmIntTest, IntBruteForceSearchKNearest) {
     auto searchAlgorithm = std::make_shared<BruteForce<int>>();
     searchAlgorithm->fit(data, distance);
     std::vector<std::pair<std::vector<int>, double>> k_nearest = searchAlgorithm->distance_to_knearest(k);
+
+    for (int i = 0; i < k_nearest.size(); i++) {
+        EXPECT_EQ(k_nearest[i].first, k_nearest_ans[i].first);
+        EXPECT_NEAR(k_nearest[i].second, k_nearest_ans[i].second, 0.01);
+    }
+}
+
+TEST_F(SearchAlgorithmDoubleTest, DoubleBruteForceSearchKNearest) {
+    auto searchAlgorithm = std::make_shared<BruteForce<double>>();
+    searchAlgorithm->fit(data, distance);
+    std::vector<std::pair<std::vector<double>, double>> k_nearest = searchAlgorithm->distance_to_knearest(k);
 
     for (int i = 0; i < k_nearest.size(); i++) {
         EXPECT_EQ(k_nearest[i].first, k_nearest_ans[i].first);
