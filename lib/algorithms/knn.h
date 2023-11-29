08e0e873db57bdd34d3588035beeae1ed48d8458
Warzkos <hubertdrozdziak@gmail.com>
2023-11-29 19:17:52 +0100

    work in progress on knn class

--- lib/algorithms/knn.h
+++ lib/algorithms/knn.h
@@ -9,13 +9,15 @@
 template <typename T>
 class KNN : public Algorithm<T> {
     public:
-        KNN(std::shared_ptr<SearchAlgorithm> searchAlgorithm, std::shared_ptr<Distance> distance);
+        KNN(std::shared_ptr<SearchAlgorithm<T>> searchAlgorithm, std::shared_ptr<Distance> distance, double contamination = 0.1, int k = 3);
 
         void fit(std::vector<std::vector<T>> data,
                     std::vector<std::vector<int>> labels = {}) override;
 
         std::vector<int> predict(std::vector<std::vector<T>> data) override;
     private:
-        std::shared_ptr<SearchAlgorithm> _searchAlgorithm;
+        std::shared_ptr<SearchAlgorithm<T>> _searchAlgorithm;
         std::shared_ptr<Distance> _distance;
+        int _k;
+        double _contamination;
 };
